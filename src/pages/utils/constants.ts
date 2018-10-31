b60d4f07ca18921194c94426b35d7158a3a847bc
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2018-10-31 14:19:15 +0100

    Tomcat URL should point to live.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- src/pages/utils/constants.ts
+++ src/pages/utils/constants.ts
@@ -7,11 +7,11 @@ import { LocalProps } from "./localprops";
 import { CoinsSender } from "./coinssender";
 
 export class Constants {    
-    static TOMCAT_URL = "http://localhost:8080";
+    //static TOMCAT_URL = "http://localhost:8080";
     static APP_VERSION = "v3.2-rc15"
     static ENABLE_GUEST = true;
     static GETH_PROXY = "http://rinkeby.xendbit.com:8546";
-    //static TOMCAT_URL = "https://lb.xendbit.com";
+    static TOMCAT_URL = "https://lb.xendbit.com";
     static RPC_PROXY = Constants.TOMCAT_URL + "/chain/x/rpc";    
     static XEND_BASE_URL = Constants.TOMCAT_URL + "/api/";
     static IMAGER_URL = Constants.TOMCAT_URL + "/imager/x/api/";
